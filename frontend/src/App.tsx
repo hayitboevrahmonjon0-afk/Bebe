1e78cdef74716863e0a0f094aa7a17b79aaee8a3
hayitboevrahmonjon0-afk <hayitboevrahmonjon0@gmail.com>
2026-08-08 17:45:32 +0500

    Create initial App component for the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <main className="app">
+      <section className="welcome">
+        <div className="bottle">🍾</div>
+
+        <h1>Spin the Bottle</h1>
+
+        <p>
+          Telegram Mini App’imizga xush kelibsiz!
+        </p>
+
+        <button>
+          O‘yinni boshlash
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
